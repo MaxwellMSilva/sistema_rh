b8a786c48b637838107725bb0e403cf49adafba7
Pauloricardorc <pauloricardorc0@gmail.com>
2021-05-16 16:08:41 -0400

    Estilização
    Foi feita o style do login, data funcionario, e exclusão dos bootstrap css, js

--- connection/database.js
+++ connection/database.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
-      host : '127.0.0.1',
-      user : 'root',
-      password : 'mydatabase',
-      database : 'database_rh'
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'database_rh'
     }
 });
 
