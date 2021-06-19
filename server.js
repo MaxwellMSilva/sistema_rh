25ec4f3aca32d85f0925bceb7356d0b796fd5344
Maxwell Silva <maxwellmachadom.m.s@gmail.com>
2021-06-19 15:08:44 -0400

    finalização do projeto RH 1.0

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
 
 app.use(express());
 app.use(routes);
